a03f79ed6fca6e61761dcc7cfd79ac33457801df
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-03-08 20:33:24 -0500

    updated apiconfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,7 @@ let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
 	// production: 'https://harmonyhaven.herokuapp.com',
-	production: 'https://web-production-bd9b.up.railway.app',
+	production: 'https://harmoney-haven-api.onrender.com',
 	development: 'http://localhost:8000',
 }
 
