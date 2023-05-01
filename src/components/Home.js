d7a7a150f7e9ea7d261dea0d3e29198c299b573f
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-05-01 10:45:09 -0400

    line breaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,9 @@ const Home = (props) => {
 				<h3 ><Link to="messageboard">Message Board</Link></h3>
 				<p>Share a post with our community. Tell us what songs you are working on or what inspires you. Leave a comment on other posts to encourge and support your fellow choir members.</p>
 				<h3><Link to="/songs">Songs</Link></h3>
-				<p>Browse through a large array of diverse repertoire located in our <Link to="/songs">song index</Link>. Choose a song from the index to see the composer, authors, lyrics, recordings, or see a video from youtoube. If you are learning a song add it to <Link to="/mysongs">My Song List</Link> so you can keep track of what songs you are learning. <Link to="/create-song">Add a song</Link> to help us grow our resources and share the music that inspires you the most.</p> 
+				<p>Browse through a large array of diverse repertoire located in our <Link to="/songs">song index</Link>. 
+				<br></br>Choose a song from the index to see the composer, authors, lyrics, recordings, or see a video from youtoube. If you are learning a song add it to <Link to="/mysongs">My Song List</Link> so you can keep track of what songs you are learning. 
+				<br></br><Link to="/create-song">Add a song</Link> to help us grow our resources and share the music that inspires you the most.</p> 
 				<h3>My Account</h3>
 				<p>Update your personal account information. <Link to="change-password">Change your password</Link> and keep it in a secure place. Keep your account secure. Don't share your password with anyone.</p> 
 			</Container>
